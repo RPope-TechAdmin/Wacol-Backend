49a193b0ca4f00f588c3bfb4e82fa43dcdd047a7
RPope-TechAdmin <rpope@purenv.au>
2026-02-23 07:29:56 +1000

    Testing if normalisation is impacting TCLP assignment

--- als-filterdata/__init__.py
+++ als-filterdata/__init__.py
@@ -590,7 +590,7 @@ def process_lab_json(data, project_no=None, workorder_code=None):
         """Normalize for reliable matching."""
         if val is None:
             return ""
-        return str(val).strip().lower().replace("(", "").replace(")", "").replace("<", "").replace(">", "").replace("~", "")
+        return str(val).strip().lower().replace("(", "").replace(")", "")
 
     pn = norm(project_no)
     wo = norm(workorder_code)
